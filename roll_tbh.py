94924f92f2359ecc188e73a2e0bd9c87cb828ea1
Vitaliy Dobrovolskiy <dobrovolskiy@denivip.ru>
2015-10-06 16:50:25 +0300

    add bear boson strategy and break after 2 successes reached

--- roll_tbh.py
+++ roll_tbh.py
@@ -25,6 +25,8 @@ def improve_fail(pool, boosts):
 
 def reroll_first_strategy(pool, boosts):
     while (boosts > 0 and (Q - 1  in pool or 5 in pool) or  6 in pool):
+        if count_successes(pool)  >= 2:
+            break
         reroll_sixes(pool)
         while 5 in pool and boosts > 0:
             boosts -= 1
@@ -40,6 +42,8 @@ def reroll_first_strategy(pool, boosts):
 
 def improve_first_strategy(pool, boosts):
     while (boosts > 0 and (Q - 1  in pool or 5 in pool)) or  6 in pool:
+        if count_successes(pool)  >= 2:
+            break
         reroll_sixes(pool)
         if Q - 1 in pool and boosts > 0:
             boosts -= 1
@@ -49,6 +53,27 @@ def improve_first_strategy(pool, boosts):
             boosts -= 1
             pool.remove(5)
             pool.append(6)
+    return pool
+
+def bear_boson_strategy(pool, boosts, reserve):
+    while boosts > 0 and (Q - 1  in pool or 5 in pool) or  6 in pool:
+        if count_successes(pool)  >= 2:
+            break
+        if Q - 1 in pool and boosts > 0:
+            boosts -= 1
+            pool.remove(Q - 1)
+            pool.append(Q)
+        elif 6 in pool and boosts >= reserve:
+            pool.remove(6)
+            pool.append(random.randint(1,6))
+            pool.append(random.randint(1,6))
+        elif 5 in pool and boosts >= reserve + 1:
+            boosts -= 1
+            pool.remove(5)
+            pool.append(random.randint(1,6))
+            pool.append(random.randint(1,6))
+        else:
+            break
     return pool
 
 
@@ -170,3 +195,63 @@ for i in range(1, 100000):
     if count_successes(roll)  >= 2:
         probability += 1
 print 'Risky strategy with 3 boosts success probability: ' + str(probability / 100000.0 * 100.0) + '%'
+
+
+### Bear boson strategy: improve fails first, improve fives only if boosts are left 1 reserve boost
+
+#Bear boson strategy roll with 1 boost
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 1, 1)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 1 boost success probability 1 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
+
+#Bear boson strategy roll with 2 boosts
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 2, 1)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 2 boosts success probability 1 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
+
+#Bear boson strategy roll with 3 boosts
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 3, 1)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 3 boosts success probability 1 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
+
+
+### Bear boson strategy: improve fails first, improve fives only if boosts are left 2 reserve boosts
+
+#Bear boson strategy roll with 1 boost
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 1, 2)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 1 boost success probability 2 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
+
+#Bear boson strategy roll with 2 boosts
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 2, 2)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 2 boosts success probability 2 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
+
+#Bear boson strategy roll with 3 boosts
+probability = 0.0
+for i in range(1, 100000):
+    roll = roll_3d6()
+    bear_boson_strategy(roll, 3, 2)
+    if count_successes(roll)  >= 2:
+        probability += 1
+print 'Bear boson strategy with 3 boosts success probability 2 reserve boost: ' + str(probability / 100000.0 * 100.0) + '%'
